8bcd5b4949240335343890f8d068c12913c36505
chereseeriepa <cherese.eriepa@outlook.com>
2022-10-07 14:16:29 +1300

    fix bug with createUrl typo

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-const test = require('tape');
+const test = require('tape')
 // const unified = require('unified');
-const remarkParse = require('remark-parse');
+const remarkParse = require('remark-parse')
 // const inspect = require('unist-util-inspect');
-const linkifyRegex = require('./index');
-const remark = require('remark');
+const linkifyRegex = require('./index')
+const remark = require('remark')
 
 const markdown = `
 # Title
@@ -17,34 +17,39 @@ This is redundantly linked: [@6ilZq3kN0F](@6ilZq3kN0F)
 cc [@alreadyLinked](@2RNGJafZt)
 `;
 
-var linkifyHashtags = linkifyRegex(/#[\w-]+/g);
+const hashtagRegex = /#[\w-]+/g
 
-test('it creates links from hashtag text without a transform function', t => {
+test('hashtag regex - creates links without transform function', t => {
   t.plan(1);
 
-  // this returns markdown content
+  
+  const linkifyHashtags = linkifyRegex(hashtagRegex)
+
   const output = remark()
       .use(linkifyHashtags)
       .use(remarkParse, { commonmark: true })
-      .processSync(markdown).contents;
+      .processSync(markdown).contents
 
-  // console.log('**output**', JSON.stringify(output, null, 2))
+  t.true(
+    output.includes('[#tag](#tag)'),
+    'should include the transformed tag URL'
+  )
+})
 
-  t.ok(output.includes('[#tag](#tag)'),
-      'should include the transformed tag URL')
-});
-
-test('creates URLs with a given function', t => {
+test('hashtag regex - creates links with transform function', t => {
   t.plan(1)
 
-  const linkifyHashtags = linkifyRegex(/#[\w-]+/g, node => {
+  const linkifyHashtags = linkifyRegex(hashtagRegex, (node) => {
     return '/foo/' + node.substring(1)
-  });
+  })
 
   const output = remark()
       .use(linkifyHashtags)
       .use(remarkParse, { commonmark: true })
-      .processSync(markdown).contents;
+      .processSync(markdown).contents
 
-    t.ok(output.includes('[#tag](/foo/tag)'))
+  t.true(
+    output.includes('[#tag](/foo/tag)'),
+    'replaces hashtag with link'
+  )
 })
