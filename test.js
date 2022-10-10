b9204150eafc2269cfceefc22026ace47ee1fa86
chereseeriepa <cherese.eriepa@outlook.com>
2022-10-10 15:54:55 +1300

    reproduce bug in a failing test

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const remarkParse = require('remark-parse')
 // const inspect = require('unist-util-inspect');
 const linkifyRegex = require('./index')
 const remark = require('remark')
+const ref = require('ssb-ref')
 
 const markdown = `
 # Title
@@ -21,7 +22,6 @@ const hashtagRegex = /#[\w-]+/g
 
 test('hashtag regex - creates links without transform function', t => {
   t.plan(1);
-
   
   const linkifyHashtags = linkifyRegex(hashtagRegex)
 
@@ -51,5 +51,31 @@ test('hashtag regex - creates links with transform function', t => {
   t.true(
     output.includes('[#tag](/foo/tag)'),
     'replaces hashtag with link'
+  )
+})
+
+
+
+test('feedId regex', t => {
+  t.plan(1)
+
+  const linkifyFeedIds = linkifyRegex(ref.feedIdRegex, (node) => {
+    return '/profile/' + node.substring(1) 
+  })
+
+  const feedIdMarkdown = `
+cc [@cherese](@Z9Su0CwHlLBmS3W6CIva67B/9oiz24MVJCpMJ4lcDmE=.ed25519)
+`;
+
+  const output = remark()
+    .use(linkifyFeedIds)
+    .use(remarkParse, { commonmark: true })
+    .processSync(feedIdMarkdown).contents
+
+  t.true(
+    output.includes(
+      '[@cherese](/profile/@Z9Su0CwHlLBmS3W6CIva67B/9oiz24MVJCpMJ4lcDmE=.ed25519)'
+    ),
+    'replaces feedId with link'
   )
 })
